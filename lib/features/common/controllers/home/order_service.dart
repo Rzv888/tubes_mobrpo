d4c2151cddf227c9ec5222c393d1781baf0cfa57
rimuri03 <rizkimr@student.telkomuniversity.ac.id>
2024-06-07 16:47:13 +0700

    update order

--- lib/features/common/controllers/home/order_service.dart
+++ lib/features/common/controllers/home/order_service.dart
@@ -6,32 +6,6 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 class OrderService {
   final supabase = Supabase.instance.client;
 
-  Future<List<Map<String, dynamic>>> getOrders() async {
-    final id = await AuthService().getUserId();
-
-    List<Map<String, dynamic>> dummyOrders = [
-      {
-        'id': '1',
-        'brandImage': 'https://via.placeholder.com/150',
-        'nama_barang': 'Air Mineral Aqua',
-        'status': 'Dalam Pengiriman',
-        'alamat': 'Sukabirus',
-        'no_wa': '08123456789',
-        'nama_lengkap': 'Udin',
-      },
-      {
-        'id': '2',
-        'brandImage': 'https://via.placeholder.com/150',
-        'nama_barang': 'Air Mineral Cleo',
-        'status': 'Dalam Pengiriman',
-        'alamat': 'Sukpur',
-        'no_wa': '08123456789',
-        'nama_lengkap': 'Cucu',
-      },
-    ];
-    return dummyOrders;
-  }
-
   Future<void> updateOrderStatus(String orderId) async {
     await Future.delayed(Duration(seconds: 1));
     print("Order status updated for orderId: $orderId");
@@ -50,10 +24,21 @@ class OrderService {
       });
       Navigator.pop(context);
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-          backgroundColor: Colors.green, content: Text('Berhasil membuat order')));
+          backgroundColor: Colors.green,
+          content: Text('Berhasil membuat order')));
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
           backgroundColor: Colors.redAccent, content: Text(e.toString())));
     }
   }
+
+  Future<List<Map<String, dynamic>>> getOrders() async {
+    final response = await supabase.from('orders').select();
+    if (response.isEmpty) {
+      return [];
+    }
+    return List<Map<String, dynamic>>.from(response);
+  }
 }
+
+
--- lib/features/common/screens/order/order.dart
+++ lib/features/common/screens/order/order.dart
@@ -1,112 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tubes_galon/common/widgets/screen_template.dart';
 import 'package:flutter_tubes_galon/features/authentication/controllers/auth_service.dart';
+import 'package:flutter_tubes_galon/features/authentication/controllers/user_service.dart';
+import 'package:flutter_tubes_galon/features/common/controllers/home/product_service.dart';
 import 'package:flutter_tubes_galon/utils/constants/sizes.dart';
 import 'package:flutter_tubes_galon/features/common/controllers/home/order_service.dart';
 
-class Order {
-  final String id;
-  final String brandImage;
-  final String brandName;
-  final String shippingStatus;
-  final String address;
-  final String phoneNumber;
-  final String name;
-
-  Order({
-    required this.id,
-    required this.brandImage,
-    required this.brandName,
-    required this.shippingStatus,
-    required this.address,
-    required this.phoneNumber,
-    required this.name,
-  });
-
-  factory Order.fromMap(Map<String, dynamic> map) {
-    return Order(
-      id: map['id'],
-      brandImage: map['brandImage'],
-      brandName: map['nama_barang'],
-      shippingStatus: map['status'],
-      address: map['alamat'],
-      phoneNumber: map['no_wa'],
-      name: map['nama_lengkap'],
-    );
-  }
-}
-
 class OrderScreen extends StatefulWidget {
-  const OrderScreen({super.key});
-
   @override
   _OrderScreenState createState() => _OrderScreenState();
 }
 
 class _OrderScreenState extends State<OrderScreen> {
-  List<Order> orders = [];
-  List<Order> history = [];
-  final OrderService orderService = OrderService();
+  final OrderService _orderService = OrderService();
+  final products = ProductService().getAllProducts();
+  final user = UserService().getCurrentUser();
+
+  late Future<List<Map<String, dynamic>>> _ordersFuture;
 
   @override
   void initState() {
     super.initState();
-    fetchOrders();
-  }
-
-  Future<void> fetchOrders() async {
-    try {
-      final id = await AuthService().getUserId();
-      if (id != null) {
-        List<Map<String, dynamic>> orderMaps = await orderService.getOrders();
-        setState(() {
-          orders = orderMaps.map((orderMap) => Order.fromMap(orderMap)).toList();
-        });
-        print("Orders fetched: ${orders.length}"); 
-      } else {
-        print("User is not logged in or user ID is null");
-      }
-    } catch (e) {
-      print('Failed to load orders: $e');
-    }
-  }
-
-  Future<void> markAsCompleted(Order order) async {
-    try {
-      await orderService.updateOrderStatus(order.id);
-      setState(() {
-        orders.remove(order);
-        history.add(order);
-      });
-    } catch (e) {
-      print('Failed to update order status: $e');
-    }
-  }
-
-  void showOrderDetails(Order order) {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text(order.brandName),
-        content: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text('${order.address}'),
-            Text('${order.phoneNumber}'),
-            Text('${order.name}'),
-          ],
-        ),
-        actions: [
-          TextButton(
-            onPressed: () async {
-              await markAsCompleted(order);
-              Navigator.of(context).pop();
-            },
-            child: Text('Selesai', style: TextStyle(color: Colors.green)),
-          ),
-        ],
-      ),
-    );
+    _ordersFuture = _orderService.getOrders();
   }
 
   @override
@@ -115,31 +30,40 @@ class _OrderScreenState extends State<OrderScreen> {
       title: 'Pesanan',
       child: Padding(
         padding: const EdgeInsets.all(AppSizes.defaultSpace),
-        child: orders.isEmpty
-            ? Center(
-                child: Text('Belum ada pesanan'),
-              )
-            : Expanded(
-                child: Container( 
-                  child: ListView.builder(
-                    itemCount: orders.length,
-                    itemBuilder: (context, index) {
-                      final order = orders[index];
-                      return Card(
-                        child: ListTile(
-                          leading: CircleAvatar(
-                            radius: 30,
-                            backgroundImage: NetworkImage(order.brandImage),
-                          ),
-                          title: Text('${order.brandName} - ${order.address}'),
-                          subtitle: Text(order.shippingStatus),
-                          onTap: () => showOrderDetails(order),
-                        ),
-                      );
-                    },
-                  ),
+        child: FutureBuilder<List<Map<String, dynamic>>>(
+          future: _ordersFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return Center(child: Text('Belum ada pesanan'));
+            } else {
+              final orders = snapshot.data!;
+              final users = snapshot.data!;
+              final products = snapshot.data!;
+              return Container(
+                height: MediaQuery.of(context).size.height, 
+                child: ListView.builder(
+                  itemCount: orders.length,
+                  itemBuilder: (context, index) {
+                    final order = orders[index];
+                    final user = users[index];
+                    final product = products[index];
+                    return Card(
+                      child: ListTile(
+                        title: Text('${product['nama_barang']}'),
+                        subtitle: Text('${order['total_transaksi']}' + '${user['alamat']}'),
+                        trailing: Text('${order['status']}'),
+                      ),
+                    );
+                  },
                 ),
-              ),
+              );
+            }
+          },
+        ),
       ),
     );
   }
